71066cae19e5a7162df9b333136fb4e5e70bc8cd
Samuel Skean <sskean2@uic.edu>
2025-10-29 14:52:31 -0500

    Added a dummy test, Github Actions to test that test.

---
+++ test/server.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+
+// TODO: Setup tests to be able to test the server, write some tests.
+describe("When2Meet Clone Server Tests", () => {
+  it("2 + 2 works", async () => {
+    expect(2 + 2).toBe(4);
+  });
+});
