43a62ba68c867efed3e0c7a437ca462f1806d46c
Samuel Skean <sskean2@uic.edu>
2025-10-29 22:51:13 -0500

    Renamed to makemeet.

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 
 // TODO: Setup tests to be able to test the server, write some tests.
-describe("When2Meet Clone Server Tests", () => {
+describe("makemeet Server Tests", () => {
   it("2 + 2 works", async () => {
     expect(2 + 2).toBe(4);
   });
