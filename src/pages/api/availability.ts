a59f97b5709eb94fc9a4c06a89905cbe4833a4b0
Samuel Skean <sskean2@uic.edu>
2025-10-31 01:46:01 -0500

    Switched from tabs to spaces.
    This seems silly, but I'm pretty sure my editor (Zed) is sometimes prone to formatting things with spaces.
    
    My guess is this is a bug with my editor, or that biome was not running at some point and my editor decided to format
    things on its own (so, arguably still a bug in my editor).
    
    Anyway I prefer spaces, if you have a strong preference the other way please just revert this commit and I'll figure out
    my issue with my editor.

--- src/pages/api/availability.ts
+++ src/pages/api/availability.ts
@@ -1,7 +1,12 @@
 export const GET = () => {
-  return new Response("This should really return availability.", { status: 405 });
+  return new Response("This should really return availability.", {
+    status: 405,
+  });
 };
 
 export const POST = () => {
-  return new Response("This should really let the users post their availability.", { status: 405 });
+  return new Response(
+    "This should really let the users post their availability.",
+    { status: 405 },
+  );
 };
