cd1fd63c086a96ac833f8f52f19dc2f40a869a85
Samuel Skean <sskean2@uic.edu>
2025-11-07 20:50:24 -0600

    Fixed api type comments, no more timezones on each date, one timezone per meeting.
    The api *should* be stable enough to write helpers for now.

--- TODO.md
+++ TODO.md
@@ -1,4 +1,5 @@
-- [ ] Decide between Unix timestamps and ISO 8601 UTC-anchored strings in the API.
+- [x] Decide between Unix timestamps and ISO 8601 UTC-anchored strings in the API.
+  - We've decided on ISO 8601 timestamps.
 - [ ] Do we want the fancy time types in our REST API/validators, or not? I can't see why we'd want to care about the fact that these ints are times on the server, so I guess its a readability vs shenanigans tradeoff more than a simplicity vs complex functionality tradeoff. And I don't see the readability argument when we can have type aliases.
 - [ ] How exactly should the no-password experience work? In when2meet, all accounts are per-meeting. I'm certain we shouldn't replicate that with OAuth/our own passkeys, but I think people should be able to enter separate names for separate meetings, and maybe they should just get a new account for each new meeting?
 - [ ] Should user IDs be unique to meetings? Kind of a big security question.
--- src/types-and-validators.ts
+++ src/types-and-validators.ts
@@ -3,19 +3,23 @@
 import * as zod from "zod/mini";
 
 // A helper for confirming that all the elements of an array of numbers or strings are unique.
-const allUnique = (arr: (number | string)[]) => new Set(arr).size === arr.length;
+const allUnique = (arr: (number | string)[]) =>
+  new Set(arr).size === arr.length;
 
-const Time = zod.string().check(zod.iso.datetime({ offset: true, precision: 0 }));
+// All timestamps in the API are ISO-8601 timestamps with second precision, ending with "Z" (meaning they are UTC time).
+const Time = zod
+  .string()
+  .check(zod.iso.datetime({ offset: false, local: false, precision: 0 }));
 
 const Times = zod.array(Time).check(zod.refine(allUnique));
 
-// TODO: Fix with info about how to do ids. https://piazza.com/class/mdt3addszda1is/post/78
+// It seems Kanich doesn't love this idea, but I (Skean) feel pretty strongly for it: https://piazza.com/class/mdt3addszda1is/post/78
 //
 // This is a JSON array of the time-slots for which one user is available for one meeting.
 // These time-slots are the beginnings of 15-minute chunks, for which the user is available.
 //
 // ```json
-// [1762195500,1762196400,1762197300,1762198200,1762199100,1762200000,1762200900,1762201800]
+// ["2025-11-03T18:45:00Z","2025-11-03T19:00:00Z","2025-11-03T19:15:00Z","2025-11-03T19:30:00Z","2025-11-03T19:45:00Z","2025-11-03T20:00:00Z","2025-11-03T20:15:00Z","2025-11-03T20:30:00Z"]
 // ```
 export const UserAvailability = Times;
 export type UserAvailability = zod.infer<typeof UserAvailability>;
@@ -24,17 +28,25 @@ export type UserAvailability = zod.infer<typeof UserAvailability>;
 // It should look something like this:
 // ```json
 // {
-//   "[userId1]" : [1762195500,1762196400,1762197300,1762198200,1762199100,1762200000,1762200900,1762201800],
-//   "[userId2]" : [1762183800,1762184700,1762185600,1762186500,1762187400,1762188300,1762189200,1762190100,1762191000]
+//   "[userId1]" : ["2025-11-03T18:45:00Z", "2025-11-03T19:00:00Z","2025-11-03T19:15:00Z","2025-11-03T19:30:00Z","2025-11-03T19:45:00Z","2025-11-03T20:00:00Z","2025-11-03T20:15:00Z","2025-11-03T20:30:00Z"],
+//   "[userId2]" : ["2025-11-03T18:45:00Z","2025-11-03T19:00:00Z","2025-11-03T19:15:00Z","2025-11-03T19:30:00Z","2025-11-03T19:45:00Z","2025-11-03T20:00:00Z","2025-11-03T20:15:00Z","2025-11-03T20:30:00Z"]
 // }
 // ```
-// That's a map from user ids to their availabilities. The availabilities are arrays of unix timestamps, each one representing a 15-minute chunk for which that person is available.
+// That's a map from user ids to their availabilities. The availabilities are arrays of timestamps, each one representing a 15-minute chunk for which that person is available.
 //
 // The order of the timestamps in the array is not significant, but we should endeavor to produce arrays with ascending timestamps. (an instance of the [Robustness Principle](https://en.wikipedia.org/wiki/Robustness_principle))
 export const MeetingAvailability = zod.map(zod.uuid(), UserAvailability);
 export type MeetingAvailability = zod.infer<typeof MeetingAvailability>;
 
-const DayOfTheWeek = zod.enum(["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]);
+const DayOfTheWeek = zod.enum([
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+  "sunday",
+]);
 export type DayOfTheWeek = zod.infer<typeof DayOfTheWeek>;
 
 const AvailableDayConstraints = zod.discriminatedUnion("type", [
@@ -63,10 +75,28 @@ export const AvailabilityContraints = zod.object({
 
 export type AvailabilityContraints = zod.infer<typeof AvailabilityContraints>;
 
+export const IanaTimezone = zod.string().check(
+  zod.refine(
+    (val) => {
+      // STRETCH: Support other locales.
+      try {
+        new Intl.DateTimeFormat("en-us", { timeZone: val });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    {
+      message: "invalid time zone",
+    },
+  ),
+);
+
 export const Meeting = zod.object({
   // Meeting names must be at least one character long.
   name: zod.string().check(zod.minLength(1)),
   availability: MeetingAvailability,
   availabilityBounds: AvailabilityContraints,
+  timeZone: IanaTimezone,
 });
 export type Meeting = zod.infer<typeof Meeting>;
