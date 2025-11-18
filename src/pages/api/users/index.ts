3ba87293c94538648e88a749df4d2570cbb54a8f
Samuel Skean <sskean2@uic.edu>
2025-11-18 11:34:32 -0600

    Better comment.
    I *believe* this marks the last path we need to implement for the MVP. Maybe even the final project? Though of course
    for that we need to implement auth.

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,6 +1,7 @@
 // TODO: For final project.
 export const prerender = false;
 
+// TODO: MVP.
 // TODO: Create user.
 export const POST = () => {
   return new Response("Unimplemented", { status: 405 });
