2890d840bd147ed70ce3a5e56e47f16542107562
Samuel Skean <sskean2@uic.edu>
2025-11-25 16:03:27 -0600

    Client-side API helpers now return id for created resources.

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,9 +1,11 @@
+import assert from "node:assert";
+
 // TODO(samuel-skean): This is a false-sense-of-security type-safety bomb waiting to go off. Basically, if you put any response into it, it will give you back whatever response you want it to return. I'm not sure of a better way to do this, but it's just a shame. I think it would honestly be better for it to return `Promise<unknown | undefined>` to force all users to cast, but I'm not willing to make that call just yet.
+// TODO(samuel-skean): The bomb went off. I defused it in this commit.
 export async function handleApiResponse<T>(
   res: Response,
   parseFn?: (json: unknown) => T,
-): Promise<T | undefined> {
-  // TODO: Samuel Skean here. We should allow passing back at least part of the location header to identify resources created in POST requests. The way I do it, the response body of a POST request never identifies the resource created, it just contains the *contents* of the resource created. In other words, the response is missing the id. But the id is always a component of the Location header of the response, and I believe it's always the last component. I'm busy at the moment or I'd do this.
+): Promise<T | (T & { id: string }) | undefined> {
   if (!res.ok) {
     // Provide a helpful error for 405 vs other errors
     if (res.status === 405) {
@@ -19,33 +21,55 @@ export async function handleApiResponse<T>(
     return undefined;
   }
 
+  let responseBody: unknown;
+
   const contentType = res.headers.get("content-type") || "";
   if (contentType.includes("application/json")) {
     const json = await res.json().catch(() => {
       throw new Error(`Response did not contain JSON.`);
     });
 
-    return parseFn ? parseFn(json) : (json as T);
-  }
-
-  // For endpoints that return plain text
-  if (contentType.startsWith("text/") || contentType === "") {
+    responseBody = json;
+  } else if (contentType.startsWith("text/") || contentType === "") {
+    // For endpoints that return plain text
     // TODO: Is this error handling achieving anything?
     const txt = await res.text().catch(() => "");
 
-    return parseFn ? parseFn(txt) : (txt as T);
+    responseBody = txt;
+  } else {
+    // Fallback - try JSON but give a helpful message on parse failures
+    try {
+      console.log(
+        "WARNING: Incorrectly set 'Content-Type', trying to parse as JSON.",
+      );
+      const json = await res.json();
+      responseBody = json;
+    } catch {
+      throw new Error(
+        `Response had unsupported content-type: ${contentType} (not JSON).`,
+      );
+    }
   }
 
-  // Fallback - try JSON but give a helpful message on parse failures
-  try {
-    console.log(
-      "WARNING: Incorrectly set 'Content-Type', trying to parse as JSON.",
-    );
-    const json = await res.json();
-    return parseFn ? parseFn(json) : (json as T);
-  } catch {
-    throw new Error(
-      `Response had unsupported content-type: ${contentType} (not JSON).`,
-    );
+  const parsedResponse = parseFn ? parseFn(responseBody) : (responseBody as T);
+
+  if (res.status === 201 /* Created */) {
+    // biome-ignore lint/style/noNonNullAssertion: If the server doesn't give us this, it is misbehaved.
+    const location = res.headers.get("location")!;
+    const locationComponentsWithoutEmptyStrings = new URL(
+      location,
+      window.location.origin,
+    ).pathname
+      .split("/")
+      .filter(Boolean);
+    const lastComponentOfLocation =
+      // biome-ignore lint/style/noNonNullAssertion: If the path doesn't have a final component, the server is misbehaved.
+      locationComponentsWithoutEmptyStrings.pop()!;
+    return {
+      ...parsedResponse,
+      id: lastComponentOfLocation,
+    };
+  } else {
+    return parsedResponse;
   }
 }
