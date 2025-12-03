946802bb7ee41d12584fa1fcb49743a6488e2521
Samuel Skean <sskean2@uic.edu>
2025-12-02 23:13:28 -0600

    Remove availability schema thing.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,4 @@
-import {
-  int,
-  primaryKey,
-  sqliteTable,
-  text,
-  unique,
-  uniqueIndex,
-} from "drizzle-orm/sqlite-core";
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const meetings = sqliteTable("meetings", {
   id: text().primaryKey(), // nanoID
@@ -16,23 +9,3 @@ export const users = sqliteTable("users", {
   id: text().primaryKey(), // nanoID
   defaultName: text().notNull(),
 });
-
-export const availabilities = sqliteTable(
-  "availabilities",
-  {
-    meetingId: text().references(() => meetings.id),
-    userId: text().references(() => users.id),
-    nameOverride: text(),
-    orderOfIdenticalNamesWithinMeeting: int(),
-  },
-  (table) => [
-    primaryKey({
-      columns: [table.meetingId, table.userId],
-    }),
-    unique("meetingId").on(
-      table.meetingId,
-      table.nameOverride,
-      table.orderOfIdenticalNamesWithinMeeting,
-    ),
-  ],
-);
