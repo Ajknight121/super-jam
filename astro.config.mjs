5c89cd7ca3d27ab32215e9b1d396dfdfdb9c9a4d
awilmo <awill275@uic.edu>
2025-12-03 19:56:31 -0600

    CSP fix calendar for real?

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,12 +28,12 @@ export default defineConfig({
       styleDirective: {
         resources: [
           "'self'",
-          'sha256-6AI67S7OdZ1tnbpP7DEcM7QG6DGH5xj8CXJv7gsgjv0=',
-          'sha256-nV/G7sFVaJB03McwJHvCSHdKsvmG5uaOg0YFokvlKGA=',
-          'sha256-m6quyuX3igpBZcDrVBilLiWjNYTfeeNFr5AzJ8Xra9U=',
-          'sha256-vv9IoKo7BSLbWcUHr3tNmfNVmm5L/9Cfn2H6LMk7/ow=',
-          'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=',
-          'sha256-Hk70poV4gtPx3YsU2xuFYNYNK6O6ARdFd3JjccYJVOA='
+          "'sha256-6AI67S7OdZ1tnbpP7DEcM7QG6DGH5xj8CXJv7gsgjv0='",
+          "'sha256-nV/G7sFVaJB03McwJHvCSHdKsvmG5uaOg0YFokvlKGA='",
+          "'sha256-m6quyuX3igpBZcDrVBilLiWjNYTfeeNFr5AzJ8Xra9U='",
+          "'sha256-vv9IoKo7BSLbWcUHr3tNmfNVmm5L/9Cfn2H6LMk7/ow='",
+          "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
+          "'sha256-Hk70poV4gtPx3YsU2xuFYNYNK6O6ARdFd3JjccYJVOA='"
         ]
       },
     }
